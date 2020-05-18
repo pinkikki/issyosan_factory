af0318f9e2292bcbbe0916df87e9976ff6cb7787
pinkikki <w-mizutani@pinkikki.jp>
2020-05-18 22:47:50 +0900

    change color

--- lib/app.dart
+++ lib/app.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:issyosan_factory/router.dart';
 import 'package:issyosan_factory/services/navigation_service.dart';
-import 'package:issyosan_factory/ui/widget/text.dart';
 
 import 'locator.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    const primaryColor = Color.fromARGB(255, 242, 132, 130);
     return MaterialApp(
       title: 'Issyosan Factory',
       theme: ThemeData(
-        textTheme: TextTheme(
-            body1: defaultBody1TextStyle, subhead: defaultSubheadTextStyle),
-        buttonTheme: ButtonThemeData(
-            shape: StadiumBorder(side: BorderSide(color: Colors.orange)),
-            buttonColor: Colors.white),
-        primarySwatch: Colors.blueGrey,
-      ),
+          textTheme: const TextTheme(),
+          buttonTheme: ButtonThemeData(
+              shape: StadiumBorder(side: BorderSide(color: Colors.orange)),
+              buttonColor: Colors.white),
+          primaryColor: primaryColor,
+          buttonColor: primaryColor,
+          floatingActionButtonTheme: const FloatingActionButtonThemeData(
+            backgroundColor: primaryColor,
+          )),
       darkTheme: ThemeData.dark(),
       initialRoute: 'login',
       navigatorKey: locator.get<NavigationService>().navigationKey,
